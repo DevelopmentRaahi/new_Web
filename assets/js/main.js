5ec346d70b12f7c2bfe0469050dc647809e09093
developertool45 <developertool45@gmail.com>
2025-07-09 16:48:35 +0530

    update menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,35 +6,53 @@ const nextBtn = document.querySelector('.next-sl');
 
 let currentSlide = 0;
 
-function updateSlidePosition() {
-  const slideWidth = slides[0].offsetWidth;
-  track.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-}
+// function updateSlidePosition() {
+//   const slideWidth = slides[0].offsetWidth;
+//   track.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+// }
 
-nextBtn && nextBtn.addEventListener('click', () => {
-  if (currentSlide < slides.length - 1) {
-	currentSlide++;
-	updateSlidePosition();
-  }
-});
+// nextBtn && nextBtn.addEventListener('click', () => {
+//   if (currentSlide < slides.length - 1) {
+// 	currentSlide++;
+// 	updateSlidePosition();
+//   }
+// });
 
-prevBtn && prevBtn.addEventListener('click', () => {
-  if (currentSlide > 0) {
-	currentSlide--;
-	updateSlidePosition();
-  }
-});
+// prevBtn && prevBtn.addEventListener('click', () => {
+//   if (currentSlide > 0) {
+// 	currentSlide--;
+// 	updateSlidePosition();
+//   }
+// });
 
-window.addEventListener('resize', updateSlidePosition);
+// window.addEventListener('resize', updateSlidePosition);
 
 
 const toggleBtn = document.getElementById('menu-toggle');
 const menu = document.querySelector('.header__menu');
+const backdrop = document.querySelector('#backdrop');
+const body = document.querySelector('body');
 
 toggleBtn.addEventListener('click', () => {
+  const isMobile = window.innerWidth <= 991; // or 768px as per your breakpoint
+
   menu.classList.toggle('active');
+  backdrop.classList.toggle('active');
+
+  if (isMobile && menu.classList.contains('active')) {
+    document.body.classList.add('no-scroll');
+  } else {
+    document.body.classList.remove('no-scroll');
+  }
 });
 
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 991) {
+    document.body.classList.remove('no-scroll');
+    menu.classList.remove('active');
+    backdrop.classList.remove('active');
+  }
+});
 document.addEventListener("DOMContentLoaded", function () {
   const track = document.querySelector('.icons-track-m');
   const slides = document.querySelectorAll('.icon-slide-m');
